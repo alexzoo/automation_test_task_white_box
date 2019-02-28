cc71cc57e76711ba1fa89a5bc1de30a7491f2833
Aleshin Aleksey <aaleshin@thumbtack.net>
2019-03-01 03:13:37 +0600

    Add bug report

--- api_tests/test_api.py
+++ api_tests/test_api.py
@@ -11,16 +11,16 @@ invalid_params = '{"error": "Invalid temperature'
 not_allowed = '{"error": "Method Not Allowed"}'
 
 """
-- Не верно реализованны возможности функции Range(есть выпавшие граничные значения).
+- Не верно реализованы возможности функции Range(есть выпавшие граничные значения).
 - Не верная обработка отрицательных значений, если не указан параметр шкалы.
-- Некорректная обработка значений выше(ниже) граничных. response = 200 and status = unknown
+- Некорректная обработка значений выше(ниже) граничных. код ответа = 200 and status = unknown(ожидаемый код ответа 403)
 - Неправильные теги для шкал Kelvin и Celsius.
-- Неверная обработка значений с длинной строки более 11. 
+- Неверная обработка значений с длинной строки более 11 символов. 
 - Неверная обработка значений со знаком '+'.
 - Неверная обработка десятичных значений.
 - Неверно обрабатывает значение None.
-- Неверная обработка значений в которых шкала с маленькой буквы.
-- Неверная обработка метод POST HTTP протокола.
+- Неверная обработка значений, в которых шкала указана с маленькой буквы.
+- Неверная обработка метода POST HTTP протокола.
 - Если в параметре передавать числовое значение без указания тега температурной шкалы, то по умолчанию API считает, что
 запрос идет в градусах цельсия. 
 - Сервер не работает со шкалой Fahrenheit.
@@ -49,7 +49,7 @@ class TestServerFunctionality(TestBaseClass):
     @pytest.mark.parametrize("default_temp, expected_response_code, condition",
                              [
                                  ('9999999999', 400, invalid_params),      # Incorrect processing of values above the boundary
-                                 ('99999999999', 400, long_string_error),  # test 11 signs - correct
+                                 ('99999999999', 400, long_string_error),  # Test 11 signs - correct
                                  ('999999999999', 400, long_string_error)  # Incorrect handling of values from a long string greater than 11
                              ])
     def test_string_length(self, default_temp, expected_response_code, condition):
@@ -64,16 +64,16 @@ class TestServerFunctionality(TestBaseClass):
                                  (' ', 400, invalid_params),
                                  (None, 400, invalid_params),       # Incorrect handling "None" values
                                  ('C', 400, invalid_params),
-                                 ('+10', 400, invalid_params),      # Incorrect handling with sign + values
+                                 ('+10', 200, liquid),              # Incorrect handling with sign + values
                                  ('10.5', 400, invalid_params),     # Incorrect handling float values
-                                 ('10с', 400, invalid_params),      # Incorrect handling small letter values
-                                 ('10 C', 400, invalid_params),
+                                 ('10с', 200, liquid),              # Incorrect handling small letter values
+                                 ('10 C', 200, liquid),
                                  ('«»‘~!@#$%^&*()?>,./\<][ /*<!—«»♣☺♂', 400, invalid_params),
                                  ('select*', 400, invalid_params),
                                  ('<script>alert("XSS1")</script>', 400, invalid_params),
                                  ('DROP TABLE temperature', 400, invalid_params),
                                  ('< form % 20 action =»http: // live.hh.ru» > < input % 20 type =»submit» > < / form >', 400, invalid_params),
-                                 ('010', 400, invalid_params),      # Incorrect handling 0 values
+                                 ('010', 200, liquid),              # Incorrect handling 0 values
                                  (' \'10\' ', 400, invalid_params)
                              ])
     def test_bad_params(self, default_temp, expected_response_code, condition):
@@ -81,19 +81,14 @@ class TestServerFunctionality(TestBaseClass):
         assert_that(response.status_code, equal_to(expected_response_code))
         assert_that(str(response.text), contains_string(condition))
 
-    def test_put_requests(self):
-        response = requests.put(self.url, params={'temperature': '10'})
-        assert_that(response.status_code, equal_to(405))
-        assert_that(str(response.text), equal_to(not_allowed))
-
-    def test_post_requests(self):
-        # Incorrect processing of methods supported by HTTP Protocol
-        response = requests.post(self.url, params={'temperature': '10'})
-        assert_that(response.status_code, equal_to(405))
-        assert_that(str(response.text), equal_to(not_allowed))
-
-    def test_delete_requests(self):
-        response = requests.delete(self.url, params={'temperature': '10'})
+    @pytest.mark.parametrize("method",
+                             [
+                                 requests.put,
+                                 requests.post,                     # Incorrect processing of methods supported by HTTP Protocol
+                                 requests.delete
+                             ])
+    def test_requests(self, method):
+        response = method(self.url, params={'temperature': '10'})
         assert_that(response.status_code, equal_to(405))
         assert_that(str(response.text), equal_to(not_allowed))
 
@@ -105,7 +100,7 @@ class TestDefaultFunctionality(TestBaseClass):
     @pytest.mark.parametrize("default_temp, expected_response_code, condition",
                              [
                                  ('999999', 200, steam),            # Range function was not used correctly, max value is absent
-                                 ('1000000', 400, invalid_params),  # Incorrect processing values above the boundary
+                                 ('1000000', 403, invalid_params),  # Incorrect processing values above the boundary
                                  ('100', 200, steam)                # Min steam - test correct
                              ])
     def test_default_steam_value(self, default_temp, expected_response_code, condition):
@@ -127,7 +122,7 @@ class TestDefaultFunctionality(TestBaseClass):
                              [
                                  ('0', 200, ice),                   # Range function was not used correctly, max value for ice(0) is absent
                                  ('-999999', 200, ice),             # Incorrect handling negative values
-                                 ('-1000000', 400, invalid_params)  # Incorrect handling negative values
+                                 ('-1000000', 403, invalid_params)  # Incorrect handling negative values
                              ])
     def test_default_ice_value(self, default_temp, expected_response_code, condition):
         response = requests.get(self.url, params={'temperature': default_temp})
@@ -143,7 +138,7 @@ class TestKelvinFunctionality(TestBaseClass):
     @pytest.mark.parametrize("default_temp, expected_response_code, condition",
                              [
                                  ('999C', 200, steam),              # Range function was not used correctly, max value is absent
-                                 ('1000C', 400, invalid_params),    # Incorrect processing values above the boundary
+                                 ('1000C', 403, invalid_params),    # Incorrect processing values above the boundary
                                  ('373.15C', 200, steam),           # Incorrect handling float values
                                  ('374C', 200, steam)               # Response steam - test correct
                              ])
@@ -167,7 +162,7 @@ class TestKelvinFunctionality(TestBaseClass):
                              [
                                  ('273.15C', 200, ice),             # Incorrect handling float values
                                  ('0C', 200, ice),                  # Absolute zero - test correct
-                                 ('-1C', 400, invalid_params)       # Incorrect processing values above the boundary
+                                 ('-1C', 403, invalid_params)       # Incorrect processing values above the boundary
                              ])
     def test_kelvin_ice_value(self, default_temp, expected_response_code, condition):
         response = requests.get(self.url, params={'temperature': default_temp})
@@ -183,7 +178,7 @@ class TestCelsiusFunctionality(TestBaseClass):
     @pytest.mark.parametrize("default_temp, expected_response_code, condition",
                              [
                                  ('999999K', 200, steam),           # Range function was not used correctly, max value is absent
-                                 ('1000000K', 400, invalid_params), # Incorrect processing values above the boundary
+                                 ('1000000K', 403, invalid_params), # Incorrect processing values above the boundary
                                  ('100K', 200, steam)               # Response steam - test correct
                              ])
     def test_celsius_steam_value(self, default_temp, expected_response_code, condition):
@@ -206,7 +201,7 @@ class TestCelsiusFunctionality(TestBaseClass):
                                  ('0K', 200, ice),                  # Range function was not used correctly, max value for ice(0) is absent
                                  ('-273.15K', 200, ice),            # Incorrect handling float values
                                  ('-999999K', 200, ice),            # Min ice - test correct
-                                 ('-1000000K', 400, invalid_params) # Incorrect processing values above the boundary
+                                 ('-1000000K', 403, invalid_params) # Incorrect processing values above the boundary
                              ])
     def test_celsius_ice_value(self, default_temp, expected_response_code, condition):
         response = requests.get(self.url, params={'temperature': default_temp})
@@ -221,7 +216,7 @@ class TestFahrenheitFunctionality(TestBaseClass):
     @pytest.mark.parametrize("default_temp, expected_response_code, condition",
                              [
                                  ('9999F', 200, steam),             # Range function was not used correctly, max value is absent
-                                 ('100000F', 400, invalid_params),  # Incorrect processing values above the boundary
+                                 ('100000F', 403, invalid_params),  # Incorrect processing values above the boundary
                                  ('212F', 200, steam)               # The scale does not work completely
                              ])
     def test_fahrenheit_steam_value(self, default_temp, expected_response_code, condition):
@@ -244,7 +239,7 @@ class TestFahrenheitFunctionality(TestBaseClass):
                                  ('32F', 200, ice),                 # The scale does not work completely
                                  ('−459.67F', 200, ice),            # Incorrect handling float values
                                  ('-9999F', 200, ice),              # The scale does not work completely
-                                 ('-10000F', 400, invalid_params)   # The scale does not work completely
+                                 ('-10000F', 403, invalid_params)   # The scale does not work completely
                              ])
     def test_fahrenheit_ice_value(self, default_temp, expected_response_code, condition):
         response = requests.get(self.url, params={'temperature': default_temp})
